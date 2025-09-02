219b3c1efe1648b64b55a02160ae55ea60b787b4
Dmitriy Krasnov <haedwyn@yandex.com>
2025-09-03 02:04:31 +0300

    Добавили фикстуры

---
+++ test_fixtures.py
@@ -0,0 +1,23 @@
+import pytest
+
+@pytest.fixture
+def browser():
+    print ("Браузер!")
+    pass
+
+@pytest.fixture
+def login_page(browser):
+    print ("Страница логина")
+    pass
+
+
+@pytest.fixture
+def user():
+    print ("Юзер")
+    return "username" , "password"
+
+
+def test_login(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
